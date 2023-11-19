feee51518cc38764e324d80f575b4dd81dc618ba
MatthewTMoncier <113633502+MatthewTMoncier@users.noreply.github.com>
2023-11-19 16:22:46 -0500

    Add files via upload

---
+++ Matthew_MadisonMoncier_Project3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Matthew_MadisonMoncier_Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 15;  i++)
+            {
+                Warehouse warehouse = new Warehouse(i);
+                warehouse.Run();
+                Thread.Sleep(1000);
+            }
+
+            
+
+
+        }
+    }
+}
